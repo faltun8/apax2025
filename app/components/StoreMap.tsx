6ede2dfff12af583c89af404bf199bbfb0bf21b3
faltun8 <frknaltn08@gmail.com>
2025-07-12 11:52:22 -0400

    fixed map

--- app/components/StoreMap.tsx
+++ app/components/StoreMap.tsx
@@ -5,12 +5,13 @@ const StoreMap: React.FC = () => {
   return (
     <section className="store-map">
       <iframe
-        src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDWfTJUX-X2vP8UkyfuwT6Uhj4oMoTec5Y&place_id=ChIJKeKxj9VP0xQRFYduweDRJH8"
+        src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDWfTJUX-X2vP8UkyfuwT6Uhj4oMoTec5Y&q=place_id:ChIJKeKxj9VP0xQRFYduweDRJH8"
         allowFullScreen
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
         className="store-map-iframe"
       ></iframe>
+
     </section>
 
   )
